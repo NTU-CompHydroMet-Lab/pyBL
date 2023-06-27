efa8906016fe2b8d074084067a31beac723771ae
PEI CHUN CHEN <94818873+Pei4579@users.noreply.github.com>
2023-06-27 14:34:24 +0800

    Add files via upload

---
+++ Runme01_Cal_stats.py
@@ -0,0 +1,51 @@
+# %%
+#5T 1H 6H 1D
+#1H 3H 6H 1D
+# datatime not datetime
+import dotmap
+from datetime import datetime as dt
+from Library.Cal_stats_lib.utils.utils import *
+from Library.Cal_stats_lib.utils.stats_calculation import *
+
+args = yaml.load(open('./config/default.yaml'), Loader=yaml.FullLoader)
+args = dotmap.DotMap(args)
+
+# step 1 get the input
+rawData = pd.read_csv('./01 Input_data/test.csv', index_col='datatime')
+rawData.index = pd.to_datetime(rawData.index)
+pDryThreshold=0.5
+
+# step 1.5 check the value
+if len(rawData.columns.values) != 1:
+    print('the raw data have more than one column!! Check again!!')
+else:
+    rawData = rawData[rawData.columns.values[0]]
+    ## delete all the negative number
+    ## assign flags
+    rawData = rawData.drop(rawData.index[rawData < 0.0])
+
+# step 2 read which kind of property
+prop = pd.read_csv(args.IO.config_path, index_col=0, header=None)
+propertyList = prop.loc['prop'].dropna().to_numpy()
+print('The used properties: {}'.format(propertyList))
+
+# step 3 read the timescale
+timeScaleList = prop.loc['time'].dropna().to_numpy()
+meanTimeScale = prop.loc['timeForMean'].dropna().to_numpy()
+outputMean = prop.loc['OutputTimeScaleForMean'].dropna().to_numpy()
+print('The time scales: {}'.format(timeScaleList))
+print('The time scale of Mean: {}'.format(meanTimeScale))
+print(f'Output time scale for Mean: {outputMean}')
+
+if outputMean[0] not in meanTimeScale.tolist():
+    print('-'*50)
+    print(f'Mean will not be calculated because {outputMean} is not in timeForMean, please choose the scale from timeForMean.')
+    
+# step 4-6 calculate and output statistical properties and their weights
+createStatisticalFile(rawData, propertyList, timeScaleList, meanTimeScale, outputMean, args.IO.stats_file_path, args.IO.weight_file_path, pDryThreshold)
+
+
+# %%
+
+
+
---
+++ Runme03_Sampling.py
@@ -0,0 +1,63 @@
+# %%
+#resample will compensate all empty timestemp
+#1h 3h 6h 24h if ih sample 5T
+
+from dotmap import DotMap
+from Library.BLRPRmodel.BLRPRx import *
+from calendar import month_abbr
+from datetime import timedelta as td
+from datetime import datetime as dt
+from datetime import datetime
+from Library.Cal_stats_lib.utils.utils import *
+from Library.Cal_stats_lib.utils.stats_calculation import *
+import numpy as np
+import pandas as pd
+import os
+
+from Library.Sampling_lib.mergeCells import *
+from Library.Sampling_lib.sampling import *
+from Library.Sampling_lib.compare import *
+from Library.Fitting_lib import fitting, objectiveFunction
+import warnings, yaml
+
+args = yaml.load(open('./config/default.yaml'), Loader=yaml.FullLoader)
+args = DotMap(args)
+args.sampling.start_time = dt.strptime(args.sampling.start_time, '%Y-%m-%d')
+args.sampling.end_time = dt.strptime(args.sampling.end_time, '%Y-%m-%d')
+
+total_sample_sta_prop = []
+num_month = pd.read_csv(args.IO.stats_file_path,index_col=0, header=0).shape[0]
+pDryThreshold = 0.5
+timeseries = []
+
+for month in range(1, num_month+1):
+    start_time, end_time, freq = args.sampling.start_time, args.sampling.end_time, args.sampling.freq
+    duration_sim_hr = (end_time - start_time).total_seconds() / 3600
+
+    # step 0 read result thetas(lambda, iota, alpha, nu, kappa, phi)
+    thetas = pd.read_csv('./02 Output_data/Theta.csv').loc[month-1]
+    print('='*50)
+    print(f'Mon : {str(month)}/{str(num_month)}')
+
+    # step 1 sample storms
+    alpha, nu, phi,storms = SampleStorm(thetas, start_time, duration_sim_hr)
+
+    #import pdb; pdb.set_trace()
+    rainfall_ts = MergeCells(storms, freq)
+    
+    # step 2 calculate the sta prop of the sample ts
+    sample_sta_prop = sample_Sta_prop(rainfall_ts, args.IO.config_path, pDryThreshold)
+    total_sample_sta_prop.append(sample_sta_prop)
+    print("----------Finish row {} sampling!----------\n\n".format(month))
+
+#  step 3 output the sta prop
+df = pd.DataFrame(total_sample_sta_prop[0])
+for i in range(1, len(total_sample_sta_prop)):
+    df2 = pd.DataFrame(total_sample_sta_prop[i])
+    fin_df = pd.concat([df, df2])
+    df = fin_df
+
+# %%
+
+
+
