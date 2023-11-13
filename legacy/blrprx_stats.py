ef43ee5f4fe6020fd7872c7afc59b253e5cf10fc
Kilin <jack233924@gmail.com>
2023-11-13 09:41:57 +0800

    Some experiment script to compare refactored and legacy code.

--- legacy/blrprx_stats.py
+++ legacy/blrprx_stats.py
@@ -1,30 +1,30 @@
 import numpy as np
 from Library.BLRPRmodel.BLRPRx import BLRPRx as LEGACY_BLRPRx
 
-from pyBL.models import BL_Props
+from pyBL.models import Stat_Props
 from pyBL.models import BLRPRx as NEW_BLRPRx
 
 theta = [
-    1.0,    # h (scale, unit=hr)
+    1.0,  # h (scale, unit=hr)
     0.016679,  # lambda
-    0.97,    # iota
-    1.0,    # sigma_mux
-    0.0,    # UNKNOWN
-    9.01,    # alpha
-    0.901,    # alpha/nu
-    0.349,    # kappa
-    0.082,   # phi
-    1.0,    # c
+    0.97,  # iota
+    1.0,  # sigma_mux
+    0.0,  # UNKNOWN
+    9.01,  # alpha
+    0.901,  # alpha/nu
+    0.349,  # kappa
+    0.082,  # phi
+    1.0,  # c
 ]
-legacy_model = LEGACY_BLRPRx(mode='gamma')
+legacy_model = LEGACY_BLRPRx(mode="gamma")
 new_model = NEW_BLRPRx()
 
 print(legacy_model.Mean(theta))
 print(legacy_model.Cov(theta))
 print(legacy_model.Var(theta))
-print(np.sqrt(legacy_model.Var(theta))/legacy_model.Mean(theta))
+print(np.sqrt(legacy_model.Var(theta)) / legacy_model.Mean(theta))
 print()
 print(new_model.mean())
 print(new_model.covariance())
 print(new_model.variance())
-print(new_model.get_prop(BL_Props.CVAR))
+print(new_model.get_prop(Stat_Props.CVAR))
--- legacy/compare_obj.py
+++ legacy/compare_obj.py
@@ -4,28 +4,36 @@ import numpy as np
 import pandas as pd
 
 from pyBL.fitting.fitter import BLRPRxFitter
-from pyBL.models import BL_Props, BLRPRx, BLRPRx_params
+from pyBL.models import Stat_Props, BLRPRx, BLRPRx_params
 from pyBL.timeseries import IntensityMRLE
 
-timescale = [1, 3600, 3*3600, 6*3600, 24*3600]
-props = [BL_Props.MEAN, BL_Props.CVAR, BL_Props.AR1, BL_Props.SKEWNESS, BL_Props.pDRY]
+timescale = [1, 3600, 3 * 3600, 6 * 3600, 24 * 3600]
+props = [
+    Stat_Props.MEAN,
+    Stat_Props.CVAR,
+    Stat_Props.AR1,
+    Stat_Props.SKEWNESS,
+    Stat_Props.pDRY,
+]
 
 # Set timezone to UTC
-os.environ['TZ'] = 'UTC'
+os.environ["TZ"] = "UTC"
+
 
 def rain_timeseries():
-    data_path = os.path.join(os.path.dirname(__file__), '01 Input_data', 'elmdon.csv')
+    data_path = os.path.join(os.path.dirname(__file__), "01 Input_data", "elmdon.csv")
     data = pd.read_csv(data_path, parse_dates=["datatime"])
-    data['datatime'] = data['datatime'].astype("int64") // 10 ** 9
-    time = data['datatime'].to_numpy()
-    intensity = data['Elmdon'].to_numpy()
+    data["datatime"] = data["datatime"].astype("int64") // 10**9
+    time = data["datatime"].to_numpy()
+    intensity = data["Elmdon"].to_numpy()
     return time, intensity
+
 
 def month_start_end():
     # Generate first day of each month from 1980 to 2010
-    day = pd.date_range(start='1980-01-01', end='2000-01-01', freq='MS')
+    day = pd.date_range(start="1980-01-01", end="2000-01-01", freq="MS")
     # Convert to unix time
-    month_srt = day.astype("int64") // 10 ** 9
+    month_srt = day.astype("int64") // 10**9
     month_end = month_srt
     # Stack them together
     month_interval = np.stack((month_srt[:-1], month_end[1:]), axis=1)
@@ -33,19 +41,24 @@ def month_start_end():
     month_interval = np.reshape(month_interval, (-1, 12, 2))
     return month_interval
 
+
 time, intensity = rain_timeseries()
-mrle = IntensityMRLE(time, intensity/3600)
+mrle = IntensityMRLE(time, intensity / 3600)
 month_interval_each_year = month_start_end()
 
 # Segment the mrle timeseries into months from 1900 to 2100
-mrle_month_each = np.empty((12, len(month_interval_each_year), 5), dtype=IntensityMRLE)    # (month, year, scale)
+mrle_month_each = np.empty(
+    (12, len(month_interval_each_year), 5), dtype=IntensityMRLE
+)  # (month, year, scale)
 for i, year in enumerate(month_interval_each_year):
     for j, month in enumerate(year):
         for k, scale in enumerate(timescale):
-            mrle_month_each[j, i, k] = mrle[month[0]:month[1]].rescale(scale)                   # 1s scale
+            mrle_month_each[j, i, k] = mrle[month[0] : month[1]].rescale(
+                scale
+            )  # 1s scale
 
 # MRLE that stores the total of each month
-mrle_month_total = np.empty((12, 5), dtype=IntensityMRLE)    # (month, scale)
+mrle_month_total = np.empty((12, 5), dtype=IntensityMRLE)  # (month, scale)
 for i in range(12):
     for j in range(len(mrle_month_each[0])):
         for k, scale in enumerate(timescale):
@@ -57,25 +70,42 @@ stats_month = np.zeros((12, 5, 5))  # (month, scale, stats)
 for month in range(12):
     for scale in range(5):
         model = mrle_month_total[month, scale]
-        stats_month[month, scale, :] = [model.mean(), model.cvar(), model.acf(), model.skewness(), model.pDry(0)]
+        stats_month[month, scale, :] = [
+            model.mean(),
+            model.cvar(),
+            model.acf(),
+            model.skewness(),
+            model.pDry(0),
+        ]
 
-stats_month_seperate = np.zeros((12, len(month_interval_each_year), 5, 5))  # (month, year, scale, stats)
+stats_month_seperate = np.zeros(
+    (12, len(month_interval_each_year), 5, 5)
+)  # (month, year, scale, stats)
 for month in range(12):
     for year in range(len(month_interval_each_year)):
         for scale in range(5):
             model = mrle_month_each[month, year, scale]
-            stats_month_seperate[month, year, scale, :] = [model.mean(), model.cvar(), model.acf(), model.skewness(), model.pDry(0)]
+            stats_month_seperate[month, year, scale, :] = [
+                model.mean(),
+                model.cvar(),
+                model.acf(),
+                model.skewness(),
+                model.pDry(0),
+            ]
 
-stats_weight = 1/np.nanvar(stats_month_seperate, axis=1)  # (month, scale, stats) (12, 5, 5)
+stats_weight = 1 / np.nanvar(
+    stats_month_seperate, axis=1
+)  # (month, scale, stats) (12, 5, 5)
 
 target = stats_month[:, 1:, :]
 weight = stats_weight[:, 1:, :]
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-params = BLRPRx_params(lambda_=0.01, phi=0.1, kappa=0.1, alpha=2.1, nu=0.3, sigmax_mux=1, iota=0.1)
+params = BLRPRx_params(
+    lambda_=0.01, phi=0.1, kappa=0.1, alpha=2.1, nu=0.3, sigmax_mux=1, iota=0.1
+)
 fitter = BLRPRxFitter()
 model = BLRPRx(params=params)
 
 print(fitter.evaluate(x=model.params(), model=model, target=target, weight=weight))
-
